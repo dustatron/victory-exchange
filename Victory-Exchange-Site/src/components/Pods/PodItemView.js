d4fdcc86a114cb3cb510cce9086d67c8d3ff9335
Dusty McCord <Dustymccord@gmail.com>
2020-05-20 14:23:43 -0700

    fix style issue in PodItemView and redirect to list view with a user leaves or joins a pod.

--- Victory-Exchange-Site/src/components/Pods/PodItemView.js
+++ Victory-Exchange-Site/src/components/Pods/PodItemView.js
@@ -22,6 +22,7 @@ function PodItemView(props) {
     if (!thisPod.users.includes(currentUser.uid)) {
       firestore.update({ collection: 'pods', doc: thisPod.podId }, { users: [ ...thisPod.users, currentUser.uid ] });
     }
+    props.withViewState(0);
   };
 
   const handleLeavePod = () => {
@@ -29,6 +30,7 @@ function PodItemView(props) {
       //remove user from pods user list
       const newUserList = thisPod.users.filter(user => user !== currentUser.uid);
       firestore.update({ collection: 'pods', doc: thisPod.podId }, { users: [ ...newUserList ] });
+      props.withViewState(0);
     }
 
     //remove pod from users profile
@@ -47,10 +49,10 @@ function PodItemView(props) {
         <Card.Subtitle className='mb-2 text-muted'> {thisPod.tagLine}</Card.Subtitle>
         <hr />
         <Row>
-          <Col dm={3}>
+          <Col md={5}>
             <Card.Img variant='top' src={thisPod.podImg} />
           </Col>
-          <Col dm={6}>
+          <Col md={6}>
             <ListGroup className='list-group-flush'>
               <ListGroupItem>Description: {thisPod.description}</ListGroupItem>
               <ListGroupItem>Location: {thisPod.location}</ListGroupItem>
@@ -60,30 +62,43 @@ function PodItemView(props) {
             </ListGroup>
           </Col>
         </Row>
-      </Card.Body>
-      <Card.Body>
-        <div style={{ display: 'flex', flexFlow: 'row', justifyContent: 'center', alignItems: 'center', alignContent: 'space-evenly' }}>
-          {/* ///////////////  Join Or Leave /////////////////// */}
-          {thisPod.users.includes(currentUser.uid) ? <Button as='input' value={currentUser.uid === thisPod.ownerId ? 'Admin' : 'Leave Pod'} onClick={handleLeavePod} /> : <Button as='input' style={{ margin: '0 10px' }} onClick={joinPod} variant='outline-success' value='Join Pod' />}
+        <Row>
+          <Col md={{ span: 8, offset: 2 }} className='text-center'>
+            {/* ///////////////  Join Or Leave /////////////////// */}
+            {thisPod.users.includes(currentUser.uid) ? (
+              <Button style={{ margin: '0 5px' }} onClick={handleLeavePod}>
+                {currentUser.uid === thisPod.ownerId ? 'Admin' : 'Leave Pod'}
+              </Button>
+            ) : (
+              <Button onClick={joinPod} variant='outline-success'>
+                Joind
+              </Button>
+            )}
 
-          {/* //////  EDIT  //// */}
-          {currentUser.uid === thisPod.ownerId ? (
-            <Button
-              as='input'
-              style={{ margin: '0 10px' }}
-              variant='outline-primary'
-              onClick={() => {
-                props.onEditClick(1);
-              }}
-              value='Edit'
-            />
-          ) : (
-            ''
-          )}
+            {/* //////  EDIT  //// */}
+            {currentUser.uid === thisPod.ownerId ? (
+              <Button
+                style={{ margin: '0 5px' }}
+                variant='outline-primary'
+                onClick={() => {
+                  props.onEditClick(1);
+                }}>
+                Edit
+              </Button>
+            ) : (
+              ''
+            )}
 
-          {/* //////  DELETE  //// */}
-          {currentUser.uid === thisPod.ownerId ? <Button as='input' style={{ margin: '0 10px' }} onClick={deletePod} variant='outline-danger' value='Delete Pod' /> : ''}
-        </div>
+            {/* //////  DELETE  //// */}
+            {currentUser.uid === thisPod.ownerId ? (
+              <Button style={{ margin: '0 5px' }} onClick={deletePod} variant='outline-danger'>
+                Delete Pod
+              </Button>
+            ) : (
+              ''
+            )}
+          </Col>
+        </Row>
       </Card.Body>
     </Card>
   );
